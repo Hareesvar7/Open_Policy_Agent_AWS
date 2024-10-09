485bdc2233ec436da11b0217bca7a0bcb751d123
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-09 23:49:29 +0530

    Update app.js

--- OpenPolicyGenerator/src/client/src/app.js
+++ OpenPolicyGenerator/src/client/src/app.js
@@ -1,1 +1,15 @@
 
+import React from 'react';
+import PolicyGenerator from './components/PolicyGenerator';
+import './styles.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>OPA Policy Generator</h1>
+            <PolicyGenerator />
+        </div>
+    );
+}
+
+export default App;
