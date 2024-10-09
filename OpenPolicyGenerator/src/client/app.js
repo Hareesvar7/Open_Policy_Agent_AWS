44d2ae4c0dad1f0242be28841ff4b75a02ee9e8a
Hari Haran N <110599490+Hareesvar7@users.noreply.github.com>
2024-10-09 23:46:50 +0530

    Create app.js

---
+++ OpenPolicyGenerator/src/client/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PolicyGenerator from './components/PolicyGenerator';
+import './styles.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>OPA Policy Generator</h1>
+            <PolicyGenerator />
+        </div>
+    );
+}
+
+export default App;
